b8472c14e82f614b83bb24658b71fe346346885a
sofia22demon <138009559+sofia22demon@users.noreply.github.com>
2025-01-20 17:50:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,8 +27,6 @@ const TelegramBot = require('node-telegram-bot-api');
 
     Despite the growing popularity, challenges persisted. A former lead of the project, who had sold off his holdings, tried to rejoin the team, causing unrest among the community. At one point, he announced a supposed “major update,” only to reveal plans for a Baby Kekius token and lock the community chat.
 
-    This move backfired, and the true supporters of Kekius Maximus decided to move forward without him. Under the leadership of @btdxscrapps, a family-oriented and hardworking individual, the community began to rebuild the token’s reputation.
-    The Rise of Kekius Maximus
 
     The turning point came when Elon Musk changed his profile picture to Kekius Maximus. This single action caused the token’s market cap to skyrocket from $300K to over $50 million in one night. The hype around $KM drew attention from new investors and cemented its position as the true OG token of its kind.
 
